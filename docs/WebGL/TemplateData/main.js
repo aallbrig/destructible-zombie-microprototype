6c5005d9a3e1833c37fc51e31c67dff9df551cfe
aallbrig <aallbrig@gmail.com>
2022-05-12 22:32:31 +0000

    Deploying to github-pages from @ aallbrig/destructible-zombie-microprototype@3419174e6808829eb5dc9a85b3cfd817cfd0650a 🚀

---
+++ docs/WebGL/TemplateData/main.js
@@ -0,0 +1,14 @@
+var canvas = document.querySelector("#unity-canvas");
+
+var config = {
+    dataUrl: "Build/WebGL.data",
+    frameworkUrl: "Build/WebGL.framework.js",
+    codeUrl: "Build/WebGL.wasm",
+    streamingAssetsUrl: "StreamingAssets",
+    companyName: "Andrew Allbright",
+    productName: "Destructible Zombie Microprototype",
+    productVersion: "0.0.3",
+    devicePixelRatio: 1,
+}
+
+createUnityInstance(canvas, config);
